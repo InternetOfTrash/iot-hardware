ee1ed360e236f3ce59fa7cbae88b5892a7d66421
HermLecluse <herm.lecluse@gmail.com>
2017-03-21 10:12:42 +0100

    Code (HardwareInterfacing) : Code commit

---
+++ InternetOfTrashHardware/InternetOfTrashHardware/ContainerLevel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetOfTrashHardware
+{ 
+    public class ContainerLevel
+    {
+        public int ID { get; set; }
+        public int FillLevel { get; set; }
+
+        public ContainerLevel(int id, int filllevel)
+        {
+            ID = id;
+            FillLevel = filllevel;
+        }
+
+        public override string ToString()
+        {
+            return ("ID = " + ID + " and FillLevel = " + FillLevel);
+        }
+    }
+}
